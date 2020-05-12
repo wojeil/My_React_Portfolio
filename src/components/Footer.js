fff098e602dc1aaf947409b439d710b70c953386
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-11 22:03:00 -0400

    fonts are active

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 
     return (
         <footer className="container-fluid">
-            <p> @Copyright <a href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a> <a href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a></p>
+            <p> @Copyright <a href="https://github.com/wojeil" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a> <a href="https://www.linkedin.com/in/wadih-ojeil/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a></p>
         </footer>
 
 
