eab113fda0a22c8cfca1ed6258e5cd0dda39eb33
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-11 21:57:55 -0400

    giving footer linkedin and github icons with links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 
     return (
         <footer className="container-fluid">
-            <p> @Copyright </p>
+            <p> @Copyright <a href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a> <a href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a></p>
         </footer>
 
 
