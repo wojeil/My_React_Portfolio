2f5486a50a32f5a6440a3082d3e5de57f0e885db
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-23 15:37:11 -0400

    Updates

---
+++ precache-manifest.e762587f2d0843b8eb7def4ba1364500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bdcc13d506f6ecbe2b6ce2f770ef28",
+    "url": "/My_React_Portfolio/index.html"
+  },
+  {
+    "revision": "74cb8b3758d06a2973b6",
+    "url": "/My_React_Portfolio/static/css/main.244b43ee.chunk.css"
+  },
+  {
+    "revision": "a9ec9662c073ae638829",
+    "url": "/My_React_Portfolio/static/js/2.2eea6ec3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My_React_Portfolio/static/js/2.2eea6ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74cb8b3758d06a2973b6",
+    "url": "/My_React_Portfolio/static/js/main.4b791c5d.chunk.js"
+  },
+  {
+    "revision": "0cd4456176c7ea8095c7",
+    "url": "/My_React_Portfolio/static/js/runtime-main.e47d5485.js"
+  }
+]);
