42f0ccf25e2617ba77cb41bc04bcbad7c89add6f
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-23 23:46:24 -0400

    added The HUB

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,13 +4,14 @@ import React from "react";
 function Cards(props) {
 
     return (
-    
+
 
         <div className="card">
             <a href={props.href} target="_blank" rel="noopener noreferrer"> <img
-                src= {props.image} className="card-img-top"
+                src={props.image} className="card-img-top"
                 alt={props.name} /> </a>
             <div className="card-body">
+                <h5 className="card-title">{props.name}</h5>
                 <p className="card-text">{props.ptag}</p>
             </div>
         </div>
