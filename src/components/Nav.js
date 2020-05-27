58dc8b5da2eff028337a040a9aa07f2d35daa7c7
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-27 15:54:12 -0400

    front page placed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,18 +6,17 @@ function Nav() {
     return (
 
         <nav className="navbar navbar-light">
-        <span className="navbar-brand  h1 ">
-            <p className="name">Wadih Ojeil</p>
-        </span>
+       
+       
         {/* <!--Nav Items have been linked--> */}
         <ul className="nav">
-            <li className="nav-item">
+            <li>
                 <a  className="nav-link" href="/">About</a>
             </li>
-            <li className="nav-item">
+            <li>
                 <a className="nav-link" href="/portfolio">Portfolio</a>
             </li>
-            <li className="nav-item">
+            <li>
                 <a className="nav-link" href="/contact">Contact</a>
             </li>
 
