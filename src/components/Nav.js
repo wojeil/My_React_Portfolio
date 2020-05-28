bd3970adee4a627925d53989db3f17a01fa0b187
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-27 21:26:15 -0400

    placed design for nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,18 +9,18 @@ function Nav() {
 
         <nav className="navbar">
         <Link className="nav-link h1 name" to="/">
-            WO
+            <span id="nav">WO</span>
         </Link>
         {/* <!--Nav Items have been linked--> */}
         <ul className="nav">
             <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/about"><span id="nav">About</span></Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/portfolio">Portfolio</Link>
+                <Link className="nav-link" to="/portfolio"> <span  id="nav" >Portfolio</span></Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/contact">Contact</Link>
+                <Link className="nav-link" to="/contact"><span  id="nav">Contact</span></Link>
             </li>
 
         </ul>
